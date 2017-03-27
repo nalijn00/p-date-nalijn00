dcf26ab5a425c107169818f4570bf9972a9bfe5c
nalijn00 <nalijn00@estudiantes.unileon.es>
2017-03-27 13:28:32 +0200

    He creado la clase Date

---
+++ Main.java
@@ -0,0 +1,6 @@
+public static void main(Strings args []){
+	Date fecha=new fecha(27,03,2017);
+	System.ouy.printl(fecha.isSameYear(2016);
+	Date fecha2=new fecha(01,01,1970);
+	System.ouy.printl(fecha.isSameYear(fecha2);
+}
